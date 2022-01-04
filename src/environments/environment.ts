e11ce786a04a3393e948a6ab61a3893a56c85d78
StoecklAlexanderPeter <51168299+StoecklAlexanderPeter@users.noreply.github.com>
2022-01-04 14:07:03 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBBoNYK1tFIjExYorSs0py0Gx1AP0759L0",
-    authDomain: "notesapp-96200.firebaseapp.com",
-    databaseURL: "https://notesapp-96200-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "notesapp-96200",
-    storageBucket: "notesapp-96200.appspot.com",
-    messagingSenderId: "294844442430",
-    appId: "1:294844442430:web:4da65ea8d3a735062bee6f"
+    //hidden
   }
 };
 
